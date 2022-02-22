12f771ca83c51e99346176ee9c40f4b3bfe701cb
unknown <alexalister1919@gmail.com>
2022-02-22 22:35:17 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const userRouter = require('./routes/userRoutes');
+const cookieParser = require('cookie-parser');
+
+const globalErrorHandler = require('./controllers/errorController');
+
+const app = express();
+
+// Body parser, reading data from body into req.body
+app.use(express.json({ limit: '10kb' }));
+app.use(express.urlencoded({ extended: true, limit: '10kb' }));
+app.use(cookieParser());
+
+// ROUTES
+app.use('/api/v1/users', userRouter);
+
+app.use((error, req, res, next) => {
+  console.log(error);
+  const status = error.statusCode || 500;
+  const message = error.message;
+  const data = error.data;
+  res.status(status).json({ message: message, data: data });
+});
+
+app.all('*', (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
+
+app.use(globalErrorHandler);
+
+module.exports = app;
